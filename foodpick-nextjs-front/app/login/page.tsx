d926f83fb0432010b6072b13b3699ffe2f3420c6
kimjang <37247073+hostkimjang@users.noreply.github.com>
2025-05-24 23:51:41 +0900

    Update page.tsx

--- foodpick-nextjs-front/app/login/page.tsx
+++ foodpick-nextjs-front/app/login/page.tsx
@@ -4,14 +4,38 @@ import React, { useState } from 'react';
 import styles from '../../styles/LoginForm.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function LoginForm() {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    alert('로그인 시도');
+
+    try {
+      const res = await signIn("credentials", {
+        id: id,
+        password: password,
+        redirect: false,
+      });
+
+      if (res?.error) {
+        setError("아이디 또는 비밀번호가 올바르지 않습니다.");
+        return;
+      }
+
+      router.push("/");
+      router.refresh();
+
+      
+    } catch (error) {
+      console.error(error);
+      setError("로그인 중 오류가 발생했습니다.");
+    }
   }
 
   return (
@@ -31,6 +55,7 @@ export default function LoginForm() {
             type="text"
             placeholder="아이디"
             value={id}
+            required
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => setId(e.target.value)}
             className={styles.inputTop}
           />
@@ -39,10 +64,12 @@ export default function LoginForm() {
             type="password"
             placeholder="비밀번호"
             value={password}
+            required
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
             className={styles.inputBottom}
           />
         </div>
+        {error && <div className={styles.error}>{error}</div>}
         <button type='submit' className={styles.loginBtn}>
           로그인 하기
         </button>
